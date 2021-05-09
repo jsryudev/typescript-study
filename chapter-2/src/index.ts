acc4d95d6c75af24dfbb3ac4600910f55fbb6ccb
JunSang Ryu <jsryu.dev@gmail.com>
2021-05-09 15:42:51 +0900

    Add chapter-2 example

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,10 @@
+console.log('Hello TypeScript');
+
+const a = 1 + 2; // number
+const b = a + 3; // number
+const c = {
+  apple: a, // number
+  banana: b, // number
+};
+
+const d = c.apple * 4; // number
