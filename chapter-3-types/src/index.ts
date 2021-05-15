ae22a038444e414a011e0e86e8bf86cf0d954753
JunSang Ryu <jsryu.dev@gmail.com>
2021-05-16 02:11:25 +0900

    Update chapter-3-types example

--- chapter-3-types/src/index.ts
+++ chapter-3-types/src/index.ts
@@ -1,3 +1,14 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable no-empty-function */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-template */
+/* eslint-disable prefer-const */
+/* eslint-disable no-var */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-self-compare */
+
 function squareOf(n: number) {
   return n * n;
 }
@@ -60,3 +71,44 @@ let dString = a + ' ' + b + c;
 let eString: string = 'zoom';
 let fString: 'john' = 'john';
 // let gString: 'john' = 'zoe'; error: gString typs is 'john'
+
+/* symbol */
+
+let aSymbol = Symbol('a'); // symbol
+let bSymbol: symbol = Symbol('b'); // symbol
+var cSymbol = aSymbol === bSymbol; // boolean
+// let d = a + 'x'; error: + operator not allow
+const eSymbol = Symbol('e');
+const fSymbol: unique symbol = Symbol('f');
+// let gSymbol: unique symbol = Symbol('g'); error: unique symbol is always `const`
+let hSymbol = eSymbol === eSymbol;
+// let gSymbol = eSymbol === fSymbol; unique symbol is always not overlap
+
+/* object */
+
+let aObject: object = {
+  b: 'x',
+}; // { b: string }
+
+let bObject = {
+  c: {
+    d: 'f',
+  },
+}; // { c: f { d: string } }
+
+let cObject: {
+  firstName: string;
+  lastName: string;
+} = {
+  firstName: 'john',
+  lastName: 'barrowman',
+};
+
+class Person {
+  constructor(
+    public firstName: string, // public eqaul this.firstName = firstName
+    public lastName: string,
+  ) {}
+}
+
+cObject = new Person('matt', 'smith');
