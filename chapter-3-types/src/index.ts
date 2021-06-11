ff9dad45688e4d6a50045d0a68d39e1d62431d10
jsryudev <jsryu.dev@gmail.com>
2021-06-12 02:16:29 +0900

    Update chapter-3-types example

--- chapter-3-types/src/index.ts
+++ chapter-3-types/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable no-empty-function */
 /* eslint-disable no-unused-expressions */
@@ -60,7 +61,7 @@ let dBigint = aBigint < 1235;
 // let eBigint = 88.5n; error: bigint is awlays integer type
 let fBigint: bigint = 100n;
 let gBigint: 100n = 100n;
-// let hBigint: bigint = 100; error: bigint is not assign 100
+// let hBigint: bigint = 100; error: bigint type cannot be assigned to 100.
 
 /* string */
 
@@ -112,3 +113,142 @@ class Person {
 }
 
 cObject = new Person('matt', 'smith');
+
+let dObject: { b: string };
+// dObject = {}; error: { b: string } Type b does not have properties required
+// dObject = { b: 1, c: 2 }; error: b property type is not number, c is not contain type
+
+let eObject: {
+  b: number;
+  c?: string;
+  [key: number]: boolean;
+};
+
+eObject = { b: 1 };
+eObject = { b: 1, c: undefined };
+eObject = { b: 1, 10: true };
+eObject = { b: 1, 10: true, 20: false };
+// eObject = { 10: true }; error: b is not contain object
+// eObject = { b: 1, 33: 'red' }; error: boolean type cannot be assigned to string type.
+
+let user: {
+  readonly firstName: string;
+} = { firstName: 'abby' };
+
+user.firstName; // string
+// user.firstName = 'abby with an e'; error: firstName is readonly
+
+let danger: {};
+danger = {};
+danger = { x: 1 };
+danger = [];
+danger = 2;
+
+/* typealias */
+type Age = number;
+type TypePerson = {
+  name: string;
+  age: Age;
+};
+
+let age: Age = 30;
+let driver: TypePerson = {
+  name: 'James',
+  age,
+};
+
+type Color = 'red';
+// type Color = 'blue'; error: duplicate type
+
+let x = Math.random() < 0.5;
+
+if (x) {
+  type Color = 'blue';
+  let aType: Color = 'blue';
+} else {
+  let bType: Color = 'red';
+}
+
+type Cat = { name: string; purrs: boolean };
+type Dog = { name: string; barks: boolean; wags: boolean };
+type CatOrDogOrBoth = Cat | Dog;
+type CatAndDpg = Cat & Dog;
+
+// Cat
+let catOrDogorBoth: CatOrDogOrBoth = {
+  name: 'Bonkers',
+  purrs: true,
+};
+
+// Dog
+catOrDogorBoth = {
+  name: 'Domino',
+  barks: true,
+  wags: true,
+};
+
+// Both
+catOrDogorBoth = {
+  name: 'Domino',
+  barks: true,
+  purrs: true,
+  wags: true,
+};
+
+// Cat and Dog
+let catAndDog = {
+  name: 'Domino',
+  barks: true,
+  purrs: true,
+  wags: true,
+};
+
+function trueOrNull(isTure: boolean) {
+  if (isTure) {
+    return 'true';
+  }
+  return null;
+}
+
+type Returns = string | null;
+
+function stringOrNumber(a: string, b: number) {
+  return a || b;
+}
+
+/* Array */
+let aArray = [1, 2, 3];
+var bArray = ['a', 'b '];
+let cArray: string[] = ['a'];
+let dArray = [1, 'a'];
+const eArray = [2, 'b'];
+
+let fArray = ['red'];
+fArray.push('blue');
+// fArray.push(true); error: boolean type cannot be assigned to string type.
+
+let gArray = [];
+gArray.push(1);
+gArray.push('red');
+
+let hArray: number[] = [];
+hArray.push(1);
+// hArray.push('red'); string type cannot be assigned to number type.
+
+let d = [1, 'a'];
+d.map((_) => {
+  if (typeof _ === 'number') {
+    return _ * 3;
+  }
+  return _.toUpperCase();
+});
+
+function buildArray() {
+  let a = [];
+  a.push(1);
+  a.push('x');
+  return a;
+}
+
+let myArray = buildArray(); // (string | number)[]
+// myArray.push(true); error: boolean type cannot be assigned to (string | number) type.
